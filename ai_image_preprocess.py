4392a6ede728e076e90186bde2d57238f1cda077
Simon Karlsson <simon.karlsson.93@hotmail.com>
2019-03-10 09:44:52 +0100

    Initial commit

---
+++ ai_image_preprocess.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+def downsize(img_arr):
+    return img_arr[::2, ::2]
+
+def rgb2gray(img_arr):
+    return np.true_divide(np.sum(img_arr, axis=-1), 3).astype(np.uint8)
+
+def normalize(img):
+    return np.array(img/255.0, dtype=np.float32)
+    
+def preprocess(img_arr):
+    downsized = downsize(img_arr)
+    gray = rgb2gray(downsized)
+    return gray
